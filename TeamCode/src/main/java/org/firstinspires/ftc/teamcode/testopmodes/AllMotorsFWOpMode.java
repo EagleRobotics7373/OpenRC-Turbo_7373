afa6221d1291332f1271509390e5764e7facc6b3
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2019-10-10 13:29:15 -0400

    added imu test opmode and developed motors FW opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testopmodes/AllMotorsFWOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testopmodes/AllMotorsFWOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.testopmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.library.functions.ExtDirMusicPlayer;
 import org.firstinspires.ftc.teamcode.library.functions.ExtMusicFile;
@@ -10,6 +11,8 @@ import org.firstinspires.ftc.teamcode.library.robot.robotcore.BasicRobot;
 public class AllMotorsFWOpMode extends OpMode {
     BasicRobot robot;
     ExtDirMusicPlayer player;
+    boolean fw = true;
+
     @Override
     public void init() {
         robot = new BasicRobot(hardwareMap);
@@ -23,10 +26,45 @@ public class AllMotorsFWOpMode extends OpMode {
             robot.frontRightMotor.setPower(1);
             robot.backLeftMotor.setPower(1);
             robot.backRightMotor.setPower(1);
-        } else robot.holonomic.stop();
+            telemetry.addData("Power", 1);
+        } else if (gamepad1.b) {
+            robot.frontLeftMotor.setPower(-1);
+            robot.frontRightMotor.setPower(-1);
+            robot.backLeftMotor.setPower(-1);
+            robot.backRightMotor.setPower(-1);
+            telemetry.addData("Power", -1);
+        } else if (Math.abs(gamepad1.left_stick_y) > 0.1) {
+            robot.frontLeftMotor.setPower(gamepad1.left_stick_y);
+            robot.frontRightMotor.setPower(gamepad1.left_stick_y);
+            robot.backLeftMotor.setPower(gamepad1.left_stick_y);
+            robot.backRightMotor.setPower(gamepad1.left_stick_y);
+            telemetry.addData("Power", gamepad1.left_stick_y);
+        } else {
+            robot.holonomic.stop();
+            telemetry.addData("Power", 0);
+        }
+
+
+
         if (gamepad1.y) {
             player.play(ExtMusicFile.UNITY);
         } else if (gamepad1.x) player.pause();
+        if (gamepad1.dpad_up) {
+            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            fw = true;
+        }
+        else if (gamepad1.dpad_down) {
+            robot.frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            robot.frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            fw = false;
+        }
+        telemetry.addData("Direction", fw?"Forward":"Reverse");
+        telemetry.update();
     }
 
     @Override
